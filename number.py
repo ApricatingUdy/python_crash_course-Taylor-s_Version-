d7c9f216b20cc177a0fdbb9fb67e2b73edb04e39
ApricatingUdy <udhaya.ksun@gmail.com>
2024-08-24 17:33:57 +0530

    Add files via upload

---
+++ number.py
@@ -0,0 +1,31 @@
+m= 2+3
+print(m)
+
+m=3-2
+print(m)
+
+m=2*3
+print(m)
+
+m=3/2
+print(m)
+
+m=3**2
+print(m)
+
+m=2 + 3**4
+print(m)
+
+m=(2+3)*4
+print(m)
+
+m= 3/2
+print(m)
+
+m= 3.0/2
+print(m)
+
+m= 3.0/2/0
+print(m)
+
+ 
